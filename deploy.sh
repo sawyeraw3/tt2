6ad3971b97b91d754b0c6916bde327d182048e4f
Sawyer Warden <sawyer.warden@gmail.com>
2018-10-26 15:44:11 -0400

    Update deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4795
+export NODEBIN=`pwd`/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
+
+echo "Building..."
+
+mkdir -p ~/.config
+
+mix deps.get
+mix compile
+(cd assets && npm install)
+(cd assets && webpack --mode production)
+mix phx.digest
+
+echo "Generating release..."
+mix release
+
+echo "Stopping old copy of app, if any..."
+_build/prod/rel/tt2/bin/tt2 stop || true
+
+echo "Starting app..."
+
+_build/prod/rel/tt2/bin/tt2 start
